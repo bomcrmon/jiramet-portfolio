e2c359e740ba9d63ce7e17255765b9c67667c06c
bomcrmon <bomcrmon@gmail.com>
2023-11-20 11:41:00 +0700

    fix

--- src/components/Social.js
+++ src/components/Social.js
@@ -7,7 +7,7 @@ class Social extends Component {
                 <a href="https://github.com/bomcrmon" target="_blank" rel="noopender noreferrer"><i class="fab fa-github" aria-hidden="true" /></a>
                 <a href="https://www.instagram.com/bomcrmon/" target="_blank" rel="noopender noreferrer"><i class="fab fa-instagram" aria-hidden="true" /></a>
                 <a href="https://www.facebook.com/bomcrmon/" target="_blank" rel="noopender noreferrer"><i class="fab fa-facebook" aria-hidden="true" /></a>
-                <a href="http://localhost:3000/contact" target="_blank" rel="noopender noreferrer"><i class="fa fa-envelope"  aria-hidden="true" /></a>
+                {/* <a href="http://localhost:3000/contact" target="_blank" rel="noopender noreferrer"><i class="fa fa-envelope"  aria-hidden="true" /></a> */}
             </div>
         );
     }
